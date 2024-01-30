d4667e279d25dc0531c488345df4353744674327
ciceroalvespro <143018315+ciceroalvespro@users.noreply.github.com>
2024-01-29 22:00:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ st.set_page_config(
 base_url = "https://raw.githubusercontent.com/ciceroalvespro/portifolio_python_streamlit/master/Dados%20publicos%20anac/dados_publicos_anac_{}.csv"
 
 # Titulo da página
-st.markdown("<h2 style='color:#004E8c'>Ranking de Aeroportos Brasileiros</h2>",
-            unsafe_allow_html=True)
-st.markdown("<span style='color:#cccc'>*Fonte: Agência Nacional de Aviação Civil - ANAC*</span>",
-            unsafe_allow_html=True)
+# st.markdown("<h2 style='color:#004E8c'>Ranking de Aeroportos Brasileiros</h2>",
+#             unsafe_allow_html=True)
+# st.markdown("<span style='color:#cccc'>*Fonte: Agência Nacional de Aviação Civil - ANAC*</span>",
+#             unsafe_allow_html=True)
 
 # lista para armazenar os arquivos
 dfs = []
